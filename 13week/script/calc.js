f6abd8ad4c3a3e5f6d61936acb63f7f5f627ee27
ryaba-ya <ksushkenchik@bk.ru>
2022-11-14 02:19:57 +0200

    homework

--- 13week/script/calc.js
+++ 13week/script/calc.js
@@ -1,24 +1,47 @@
-function calcPlus(){
-    let operand1 = prompt('First number');
-    let operand2 = prompt('second number');
-    alert (   +operand1 + +operand2)
-}
-function calcMinus(){
-    let operand1 = prompt('First number');
-    let operand2 = prompt('second number');
-    alert (   operand1 - operand2);
-}function calcTimes(){
-    let operand1 = prompt('First number');
-    let operand2 = prompt('second number');
-    alert (   +operand1 * +operand2);
-}function calcDivide(){
-    let operand1 = prompt('First number');
-    let operand2 = prompt('second number');
-   let result=  operand1 / operand2;
+let operator;
+
+
+        function plus() {
+            let num1, num2, result;
+            num1 = document.querySelector('#number1').value;
+            num1 = parseInt(num1); //преобразование в число
+            num2 = document.querySelector('#number2').value;
+            num2 = parseInt(num2);
+            result = num1 + num2;
+            document.querySelector('#out').innerHTML = result; // в содержимое элемента <p></p> мы помещаем результат
+        }
+        function minus() {
+            let num1, num2, result;
+            num1 = document.querySelector('#number1').value;
+            num1 = parseInt(num1);
+            num2 = document.querySelector('#number2').value;
+            num2 = parseInt(num2);
+            result = num1 - num2;
+            document.querySelector('#out').innerHTML = result;
+        }
+        function times() {
+            let num1, num2, result;
+            num1 = document.querySelector('#number1').value;
+            num1 = parseInt(num1);
+            num2 = document.querySelector('#number2').value;
+            num2 = parseInt(num2);
+            result = num1 * num2;
+            document.querySelector('#out').innerHTML = result;
+        }
+        function divide() {
+            let num1, num2, result;
+            num1 = document.querySelector('#number1').value;
+            num1 = parseInt(num1);
+            num2 = document.querySelector('#number2').value;
+            num2 = parseInt(num2);
+            result = num1 / num2;
+            if (result===Infinity){
+                document.querySelector('#out').innerHTML = "Делить на ноль нельзя"
+               } else{
+                document.querySelector('#out').innerHTML = result;
+               };
+            
+        }
 //    Если значение результата === бесконечности, тогде вывести Делить на ноль нельзя
-   if (result===Infinity){
-    alert("Делить на 0 нельзя");
-   } else{
-    alert(result)
-   };
-}
+   
+
