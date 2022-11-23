7cf8959222a209d739eba6051a55ced648637363
ryaba-ya <ksushkenchik@bk.ru>
2022-11-23 04:06:41 +0200

    valid

--- 15week/script.js/signin.js
+++ 15week/script.js/signin.js
@@ -1,1 +1,96 @@
 "use strict"
+//const mainForm = document.forms.main;
+//console.log(mainForm.elements);
+//const userNameInput = mainForm.name;
+//console.log(userNameInput.value);
+//const formSelect = mainForm.age;
+//const numberChosen = formSelect.selectedIndex;
+//console.log(numberChosen)
+//const selectValueText = formSelect.options[numberChosen].text;
+//console.log(selectValueText)
+//formSelect.options[2].selected=true;
+//let newOption= new Option("5", 5,false,false);
+//formSelect.append(newOption)
+//const userNameInputPlaceholder =userNameInput.placeholder;
+//console.log(userNameInputPlaceholder)
+
+//userNameInput.addEventListener("focus", function(e){
+   //userNameInputPlaceholder="";
+//});
+//const emailInput = mainForm.email;
+//mainForm.addEventListener("submit", function(event){
+   // if(emailTest(emailInput)){
+       // emailInput.parentElement.insertAdjacentHTML(
+           // "beforeend",
+           // `<divclass="main-form__error">
+           // write ypur email
+           // </div>`
+       // );
+       // event.preventDefault();
+   // }
+//})
+//function emailTest(input){
+   // return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+//}
+
+//let error = formValidate(mainForm);
+
+//function formValidate(mainForm){
+
+//}
+
+
+
+//mainForm.addEventListener("submit", function(event){
+   //console.log("sending");
+
+    //if(! userNameInput.value){
+       // console.log('put something');
+       // event.preventDefault();
+   // }
+//})
+//mainForm.addEventListener(submit)
+
+
+
+const form = document.forms.main;
+const formInputs = document.querySelectorAll(".warning");
+const inputEmail = form.email;
+const inputName = form.name;
+const inputCheckbox = form.agreement;
+
+form.addEventListener('submit', function(event){
+    console.log("sending!");
+let emailValue = inputEmail.value;
+let nameValue = inputName.value;
+let emptyInputs = Array
+
+formInputs.forEach(function (input) {
+    if(input.value ===""){
+        input.parentElement.insertAdjacentHTML(
+             "beforeend",
+            `<div class="main-form__error">
+             Это поле обязательно для заполнения!
+             </div>`
+         );
+        event.preventDefault();
+        console.log("input not filled");
+    } else {formInputs.classList.remove('main-form__error');
+
+    }
+})
+
+    return false;
+})
+//const emailInput = mainForm.email;
+//mainForm.addEventListener("submit", function(event){
+   // if(emailTest(emailInput)){
+       // emailInput.parentElement.insertAdjacentHTML(
+           // "beforeend",
+           // `<divclass="main-form__error">
+           // write ypur email
+           // </div>`
+       // );
+       // event.preventDefault();
+   // }
+//})
