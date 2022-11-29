21c98dfad898767aaa47382f4afb2e9f5233f07d
ryaba-ya <ksushkenchik@bk.ru>
2022-11-29 03:39:16 +0200

    calc

--- 16week/script/calc2.js
+++ 16week/script/calc2.js
@@ -1,19 +1,18 @@
 document.forms.publish.onsubmit = function () {
     let message = this.message.value;
 
-
     let arr = message.split(',')
     //console.log(message)
     //console.log(typeof arr);
+    
 
     function compareNumber(a, b) {
         return a - b
     }
     console.log(arr.sort(compareNumber));
 
-
-    let sumArray = arr.reduce(function (previousValue, item, index, array) {
-        return item + previousValue;
+    let sumArray = arr.reduce( (accumulator, item, index, array) {
+        return item + accumulator;
     }, 0);
     console.log(sumArray);
 
