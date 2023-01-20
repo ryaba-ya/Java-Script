b53a2b9f713fd9601e9bcfdb06bacc35f6f2dd24
ryaba-ya <ksushkenchik@bk.ru>
2023-01-20 23:41:23 +0200

    format

--- 13week/script/calc.js
+++ 13week/script/calc.js
@@ -1,23 +1,14 @@
-let operator;
-
-
-       
-        
-//    Если значение результата === бесконечности, тогде вывести Делить на ноль нельзя
-   let num1 = document.querySelector(".a")
-   console.log(num1)
-
+let num1 = document.querySelector(".a")
 let num2 = document.querySelector(".b")
-
 let result = document.querySelector(".result")
-let buttonPlus= document.querySelector(".plus")
-let buttonMinus= document.querySelector(".minus")
-let buttonTimes= document.querySelector(".times")
-let buttonDivide= document.querySelector(".divide")
+let buttonPlus = document.querySelector(".plus")
+let buttonMinus = document.querySelector(".minus")
+let buttonTimes = document.querySelector(".times")
+let buttonDivide = document.querySelector(".divide")
 let cleanButton = document.querySelector(".clean")
 
 class Calc {
-    static sum(){
+    static sum() {
         let a = num1.value
         a = parseInt(a)
         let b = Number(num2.value)
@@ -26,16 +17,16 @@ class Calc {
         result.innerHTML = result1
 
     }
-    static minus(){
+    static minus() {
         let a = Number(num1.value);
-       
+
         let b = Number(num2.value);
         let result1 = a - b;
         console.log(result1);
-        document.querySelector(".result").innerHTML = result1; 
+        document.querySelector(".result").innerHTML = result1;
 
     }
-    static times(){
+    static times() {
         let a = num1.value
         a = parseInt(a)
         let b = Number(num2.value)
@@ -44,30 +35,30 @@ class Calc {
         result.innerHTML = result1
 
     }
-    static divide(){
+    static divide() {
         let a = num1.value
         a = parseInt(a)
         let b = Number(num2.value)
         let result1 = a / b
         console.log(result1)
-        if(result1===Infinity){
-            result.innerHTML="Делить на ноль нельзя"
-        }else{
+        if (result1 === Infinity) {
+            result.innerHTML = "Делить на ноль нельзя"
+        } else {
             result.innerHTML = result1
         }
-       
-        
+
+
 
     }
-    static clean(){
-        num1.value="";
-        num2.value="";
-        result.innerHTML="";
+    static clean() {
+        num1.value = "";
+        num2.value = "";
+        result.innerHTML = "";
     }
 }
 
-buttonPlus.addEventListener("click",Calc.sum)
-buttonMinus.addEventListener("click",Calc.minus)
-buttonTimes.addEventListener("click",Calc.times)
-buttonDivide.addEventListener("click",Calc.divide)
-cleanButton.addEventListener("click",Calc.clean)
+buttonPlus.addEventListener("click", Calc.sum)
+buttonMinus.addEventListener("click", Calc.minus)
+buttonTimes.addEventListener("click", Calc.times)
+buttonDivide.addEventListener("click", Calc.divide)
+cleanButton.addEventListener("click", Calc.clean)
