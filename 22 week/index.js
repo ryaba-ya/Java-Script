c2968eed3246388b9eaeb3bd391c03a5a4124924
ryaba-ya <ksushkenchik@bk.ru>
2023-02-08 23:53:28 +0200

    npm

---
+++ 22 week/index.js
@@ -0,0 +1,10 @@
+try {
+	if(!parseInt('ыыыы')===NaN) {
+console.log(e.name);
+console.log(e.message);
+	}
+	console.log(parseInt('ыыыы'));
+} catch (e) {
+	console.log(e.name);
+	console.log(e.message);
+}
