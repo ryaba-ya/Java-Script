0702e8e04a4c3a908680f0bda3161bc2c912bc8a
ryaba-ya <ksushkenchik@bk.ru>
2023-02-15 01:44:06 +0200

    npm

---
+++ 22week/index.js
@@ -0,0 +1,31 @@
+const moment = require("moment");
+moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(moment())
+const Chart = require("./node_modules/chart.js/dist/chart.umd");
+(async function() {
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+  
+    new Chart(
+      document.getElementById('acquisitions'),
+      {
+        type: 'bar',
+        data: {
+          labels: data.map(row => row.year),
+          datasets: [
+            {
+              label: 'Acquisitions by year',
+              data: data.map(row => row.count)
+            }
+          ]
+        }
+      }
+    );
+  })();
