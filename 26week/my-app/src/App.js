af42682b7f028fab601495b9479960ec30ce5fe1
ryaba-ya <ksushkenchik@bk.ru>
2023-03-16 23:26:29 +0200

    Hero component

--- 26week/my-app/src/App.js
+++ 26week/my-app/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
 import './components/hero'
+import Hero from './components/hero';
 
 class App extends React.Component  {
   render(){
   return (
     <div className="App">
-      <div className="hero-card">
-       
-        <div className="hero-name">Spider-Man</div>
-        <div className="hero-text">Lorem ipsun</div>
-        <button> Take home</button>
-      </div>
+     <Hero></Hero>
+     <Hero></Hero>
       
     </div>
   );
---
+++ 26week/my-app/src/components/hero.jsx
@@ -0,0 +1,11 @@
+import './hero';
+function Hero (){
+    return(
+        <div className='hero-card'>
+            <div className='hero-name' >Spiderman</div>
+            <div className='hero-text'></div>
+            <button>Take home</button>
+        </div>
+    )
+}
+export default Hero;
