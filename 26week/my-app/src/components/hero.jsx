11181c94f13eaa6cd427414350ceafcb8e704022
ryaba-ya <ksushkenchik@bk.ru>
2023-03-17 22:12:02 +0200

    bootstrap

--- 26week/my-app/src/components/hero.jsx
+++ 26week/my-app/src/components/hero.jsx
@@ -3,7 +3,7 @@ import './hero.css';
 function Hero (props){
     return(
         <div className='hero-card'>
-            <img className='hero-img' src={props.url}/>
+            <img className='hero-img col-md-9' src={props.url}/>
             <div className='hero-name' >{props.name}</div>
             <div className='hero-text'></div>
             <button>Take home</button>
