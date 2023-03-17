a4fcd75e526a3324ebc58cc7a7418c42aa251064
ryaba-ya <ksushkenchik@bk.ru>
2023-03-17 22:49:19 +0200

    try

--- 26week/my-app/src/components/bootstrap.js
+++ 26week/my-app/src/components/bootstrap.js
@@ -2,14 +2,12 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from 'react-bootstrap/Button';
 
-
-
-function BasicExample() {
+function BasicExample(props) {
     return (
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={props.url} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>{props.name}</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
