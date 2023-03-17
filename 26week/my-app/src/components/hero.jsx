b371c0add7f28bef41cf4666fbf805988476276b
ryaba-ya <ksushkenchik@bk.ru>
2023-03-18 00:00:00 +0200

    css

--- 26week/my-app/src/components/hero.jsx
+++ 26week/my-app/src/components/hero.jsx
@@ -4,10 +4,12 @@ function Hero (props){
     return(
         <div className='hero-card'>
             <img className='hero-img ' src={props.url}/>
-           
-            <div className='hero-name' >{props.name}</div>
+           <div className='card-body'>
+           <div className='hero-name' >{props.name}</div>
             <div className ="hero-text">{props.text}</div>
             <div className='hero-text'></div>
+           </div>
+            
             
            
             
