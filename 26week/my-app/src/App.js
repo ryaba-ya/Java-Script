5ae729025a9dd463140af5a23daa4d8ac25b24a8
ryaba-ya <ksushkenchik@bk.ru>
2023-03-17 23:37:28 +0200

    try float

--- 26week/my-app/src/App.js
+++ 26week/my-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import './components/hero'
 import Hero from './components/hero';
-import KitchenSinkExample from "./components/bootstrap-card"
+
 
 
 class App extends React.Component  {
@@ -11,7 +11,7 @@ class App extends React.Component  {
     <div className="App">
      <Hero name="Spiderman" url="https://i0.wp.com/comicbookdispatch.com/wp-content/uploads/2021/12/img_3412.jpg?fit=675%2C1024&ssl=1"></Hero>
      <Hero name="Betman" url="https://sothebys-md.brightspotcdn.com/0f/56/3e0051094b1c9cfacf4fa1f608f3/023l20895-bj8tp.jpg"  ></Hero>
-<KitchenSinkExample url="https://sothebys-md.brightspotcdn.com/0f/56/3e0051094b1c9cfacf4fa1f608f3/023l20895-bj8tp.jpg"></KitchenSinkExample>
+
     </div>
   );
 }
