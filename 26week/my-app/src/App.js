9e00628d88c52f3ae9c01ff84a1fe0614d5e86e2
ryaba-ya <ksushkenchik@bk.ru>
2023-03-17 23:10:32 +0200

    вудуеу ищще

--- 26week/my-app/src/App.js
+++ 26week/my-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import './components/hero'
 import Hero from './components/hero';
-import BasicExample from './components/bootstrap';
+
 
 class App extends React.Component  {
   render(){
@@ -10,7 +10,7 @@ class App extends React.Component  {
     <div className="App">
      <Hero name="Spiderman" url="https://i0.wp.com/comicbookdispatch.com/wp-content/uploads/2021/12/img_3412.jpg?fit=675%2C1024&ssl=1"></Hero>
      <Hero name="Betman" url="https://sothebys-md.brightspotcdn.com/0f/56/3e0051094b1c9cfacf4fa1f608f3/023l20895-bj8tp.jpg" ></Hero>
-    <BasicExample></BasicExample>
+
     </div>
   );
 }
