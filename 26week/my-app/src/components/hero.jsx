47132dad88cba90d4413da97871a05dfba33dced
ryaba-ya <ksushkenchik@bk.ru>
2023-03-17 22:25:46 +0200

    delet boot

--- 26week/my-app/src/components/hero.jsx
+++ 26week/my-app/src/components/hero.jsx
@@ -1,19 +1,17 @@
 import './hero.css';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
 function Hero (props){
     return(
-        <Card className='hero-card'>
-            <Card.img variant='top' className='hero-img ' src={props.url}/>
-            <Card.Body>
-            <Card.Text className='hero-name' >{props.name}</Card.Text>
+        <div className='hero-card'>
+            <img variant='top' className='hero-img ' src={props.url}/>
+           
+            <div className='hero-name' >{props.name}</div>
             <div className='hero-text'></div>
-            <Button>Take home</Button>
-            </Card.Body>
+            <button>Take home</button>
+           
             
             
-        </Card>
+        </div>
     )
 }
 
