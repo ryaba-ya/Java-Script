7e7b34ad903cdb529f90d95108c7c00ba989232d
ryaba-ya <ksushkenchik@bk.ru>
2023-03-18 00:38:31 +0200

    css

--- 26week/my-app/src/components/hero.jsx
+++ 26week/my-app/src/components/hero.jsx
@@ -3,17 +3,33 @@ import './hero.css';
 function Hero (props){
     return(
         <div className='hero-card'>
-            <img className='hero-img ' src={props.url}/>
+            <img className='hero-img ' src={props.img}/>
            <div className='card-body'>
            <div className='hero-name' >{props.name}</div>
-            <div className ="hero-text">{props.text}</div>
-            <p className="heroes-superpowers">
+            <div className ="hero-text">{props.details}</div>
+            <div className="heroes-superpowers">
+                <span>Вселенная: </span>
+                {props.universe}
+            </div>
+            <div className="heroes-superpowers">
+                <span>Альтер эго: </span>
+                {props.alterEgo}
+            </div>
+            <div className="heroes-superpowers">
+                <span>Род деятельности:</span>
+                {props.occupation}
+            </div>
+            <div className="heroes-superpowers">
+                <span>Друзья: </span>
+                {props.friends}
+            </div>
+            <div className="heroes-superpowers">
                 <span>Суперсилы: </span>
-                {props.superpowers}
-            </p>
+                {props.superPower}
+            </div>
            </div>
-            
-            
+          
+           
            
             
             
