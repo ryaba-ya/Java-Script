a1ea3fc8890973660880d133664fc8c9191628b9
ryaba-ya <ksushkenchik@bk.ru>
2023-03-25 23:03:05 +0200

    css

--- 27week/cards-test/src/components/Card.jsx
+++ 27week/cards-test/src/components/Card.jsx
@@ -2,11 +2,11 @@ import './card.css';
 function Card(props) {
     return (
         <div className='card-wrap'>
-<header className='tariff-name'>{props.name}</header>
-<main className='price '>{props.price} руб/мес</main>
+<header className={`tariff-name ${props.color}`}>{props.name}</header>
+<main className={`price ${props.color} `}>{props.price} руб/мес</main>
 <div className='tariff-speed'> до{props.speed}  Мбит/сек</div>
 <footer className='card-footer'>{props.footer_text}</footer>
-<color className={props.color}></color>
+
         </div>
     )
 }
