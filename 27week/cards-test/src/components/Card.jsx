a87efcef47b82ff26f71bf950f6f0346c58964ce
ryaba-ya <ksushkenchik@bk.ru>
2023-03-25 22:48:17 +0200

    color

--- 27week/cards-test/src/components/Card.jsx
+++ 27week/cards-test/src/components/Card.jsx
@@ -3,9 +3,10 @@ function Card(props) {
     return (
         <div className='card-wrap'>
 <header className='tariff-name'>{props.name}</header>
-<main className='price'>{props.price} руб/мес</main>
+<main className='price '>{props.price} руб/мес</main>
 <div className='tariff-speed'> до{props.speed}  Мбит/сек</div>
 <footer className='card-footer'>{props.footer_text}</footer>
+<color className={props.color}></color>
         </div>
     )
 }
