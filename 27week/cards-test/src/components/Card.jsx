243c271a31fc5922bc0faeb8e82d4a0ced28372b
ryaba-ya <ksushkenchik@bk.ru>
2023-03-25 21:55:33 +0200

    cards

---
+++ 27week/cards-test/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import './card.css';
+function Card(props) {
+    return (
+        <div className='card-wrap'>
+<header className='tariff-name'>{props.name}</header>
+<main className='price'>{props.price} руб/мес</main>
+<div className='tariff-speed'> до{props.speed}  Мбит/сек</div>
+<footer className='card-footer'>{props.footer_text}</footer>
+        </div>
+    )
+}
+
+export default Card;
