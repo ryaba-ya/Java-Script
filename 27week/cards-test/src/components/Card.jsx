66a7e9dd80a56fbfb3761f3fc169e451cdbaaac6
ryaba-ya <ksushkenchik@bk.ru>
2023-05-10 23:21:36 +0300

    select

--- 27week/cards-test/src/components/Card.jsx
+++ 27week/cards-test/src/components/Card.jsx
@@ -1,11 +1,12 @@
-import './card.css';
+import './card.scss';
 function Card(props) {
+    const {isSelected } = props;
     return (
-        <div className={`card-wrap ${props.selected}`}>
+        <div className={"card-wrap"+ (isSelected?"selected":"")}>
 <header className={`tariff-name ${props.color}`}>{props.name}</header>
 <main className={`price ${props.color} `}>{props.price} руб/мес</main>
 <div className='tariff-speed'> до{props.speed}  Мбит/сек</div>
-<footer className='card-footer grey'>{props.footer_text}</footer>
+<footer className='card-footer grey'>{props.footerText}</footer>
 
         </div>
     )
