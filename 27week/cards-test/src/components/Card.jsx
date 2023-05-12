559682b1f99aeba1e5954fe266590bdfac38ab3b
ryaba-ya <ksushkenchik@bk.ru>
2023-05-12 21:43:25 +0300

    misatake

--- 27week/cards-test/src/components/Card.jsx
+++ 27week/cards-test/src/components/Card.jsx
@@ -1,8 +1,8 @@
 import './card.scss';
 function Card(props) {
-    const {isSelected, } = props;
+    const {isSelected } = props;
     return (
-        <div className={"card-wrap"+ (isSelected?"selected":"")}>
+        <div className={`card-wrap ${props.selected}`}>
 <header className={`tariff-name ${props.color}`}>{props.name}</header>
 <main className={`price ${props.color} `}>{props.price} руб/мес</main>
 <div className='tariff-speed'> до{props.speed}  Мбит/сек</div>
