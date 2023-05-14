6ef48351a32bede00babd308c46b4359651c1439
ryaba-ya <ksushkenchik@bk.ru>
2023-05-14 23:04:36 +0300

    н

--- Wordle/eng-app/src/components/Table/words_list.jsx
+++ Wordle/eng-app/src/components/Table/words_list.jsx
@@ -2,19 +2,13 @@ import React from 'react';
 
 const Table = (props) => {
     const words = require('../../Words.json');
-  
-    const {words,isEdit}=props;
+
     const tableRows = words.map((row)=>(
         <tr key={row.id}>
       <td>{row.english}</td>
       <td>{row.transcription}</td>
       <td>{row.russian}</td>
-      {isEdit && (
-              <td>
-                <button>Edit</button>
-                <button>Delete</button>
-              </td>
-            )}
+     
     </tr>
     ))
   return (
@@ -24,7 +18,7 @@ const Table = (props) => {
           <th>English</th>
           <th>Transcription</th>
           <th>Russian</th>
-          {isEdit && <th>Actions</th>}
+         
         </tr>
       </thead>
       <tbody>
